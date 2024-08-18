390af38463f73baafa2a86150da5f8d226f640f7
Juan Pablo Magalhães Felix Vieira <165536735+JuanPMFelix@users.noreply.github.com>
2024-08-18 11:29:26 -0300

    Atualizar o convert_temp_pt-br.c

--- convert_temp_pt-br.c
+++ convert_temp_pt-br.c
@@ -7,19 +7,29 @@
 
 int main (void)
 {
-    char t = get_char("Você quer converter para Fahrenheit ou Celsius? \n F ou C? ");
+    int a;
+    do
+    {
+        char t = get_char("Você quer converter para Fahrenheit ou Celsius? \n F ou C? ");
 
-        // De Celsius para Fahrenheit
-    if (t == 'F' || t == 'f')
-    {
-        double c = get_double("Digite a temperatura em Celsius: ");
-        printf("%.2f°C = %.2f°F \n", c, 9 * c / 5 + 32);
+            // De Celsius para Fahrenheit
+        if (t == 'F' || t == 'f')
+        {
+            double c = get_double("Digite a temperatura em Celsius: ");
+            printf("%.2f°C = %.2f°F \n", c, 9 * c / 5 + 32);
+            a = 1;
+        }
+            // De Fahrenheit para Celsius
+        else if (t == 'C' || t == 'c')
+        {
+            double f = get_double("Digite a temperatura em Fahrenheit: ");
+            double r = 5 * f - 160; // Tive que separar a operação em duas linhas, pois em uma só dava erro. O computador acabava aproximando o 160/9 :(
+            printf("%.2f°F = %.2f°C \n", f, r / 9);
+            a = 1;
+        }
     }
-        // De Fahrenheit para Celsius
-    else if (t == 'C' || t == 'c')
-    {
-        double f = get_double("Digite a temperatura em Fahrenheit: ");
-        double r = 5 * f - 160; // Tive que separar a operação em duas linhas, pois em uma só dava erro. O computador acabava aproximando o 160/9 :(
+    while (a == 1);
+}, pois em uma só dava erro. O computador acabava aproximando o 160/9 :(
         printf("%.2f°F = %.2f°C \n", f, r / 9);
     }
 }
